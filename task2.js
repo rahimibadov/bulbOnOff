4977ef7539a49e1a9a70ead49adcae75a63ef3fe
rahimibadov <rahim.ibadov@gmail.com>
2024-05-24 16:15:21 -0600

    first commit

---
+++ task2.js
@@ -0,0 +1,12 @@
+
+let lampImg = document.querySelector("img");  
+
+function switchOnLight() {  
+    lampImg.src = "./img/bulbON.jpg"; 
+}
+
+function switchOffLight() {  
+    lampImg.src = "./img/bulbOFF.jpg"; 
+}
+
+
